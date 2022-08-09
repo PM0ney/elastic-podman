3de4c10423c52989a8d65a17a373103db3916476
PM0ney <preston62091@gmail.com>
2022-08-09 09:50:33 -0400

    updated default password

--- elasticsearch/setup_password.sh
+++ elasticsearch/setup_password.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-curl -X POST -k -u elastic:henrylabelastic "https://localhost:9200/_security/user/kibana_system/_password?pretty" -H 'Content-Type: application/json' -d'
+curl -X POST -k -u elastic:password "https://localhost:9200/_security/user/kibana_system/_password?pretty" -H 'Content-Type: application/json' -d'
 {
-  "password" : "henrylabelastic"
+  "password" : "password"
 }
 '
